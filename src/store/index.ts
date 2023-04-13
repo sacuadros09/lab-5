df0f9f62ccf0fddc23871286455d81ab57240e4e
sacuadros09 <sacuma09@gmail.com>
2023-04-12 20:19:40 -0500

    final hemosho

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,7 +10,7 @@ let observed: Observed[] =[];
 export const dispatch =(action:Actions) =>{
     const clone = JSON.parse(JSON.stringify(appState))
     appState = reducer(action,clone);
-    observed.forEach(o => o.render());
+    observed.forEach(s => s.render());
 }
 
 export const addObserved = (ref: Observed) => {observed = [...observed,ref]};
